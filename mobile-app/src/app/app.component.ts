ca32b927ee6258894d019d9a8fb5364f1ac82ad1
Domitien VISSOTSKY <domitien.vissotsky@student.vinci.be>
2020-12-03 13:28:58 +0100

    scan button init

--- mobile-app/src/app/app.component.ts
+++ mobile-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'BlockCovid App';
+  constructor(private router : Router) { }
+  scanHandler(){
+    
+    // http request get id 
+    // stocker quelque part 
+    // if id recu reroute si pas on reste 
+    this.router.navigate(['/scanqr'])
+  }
 }

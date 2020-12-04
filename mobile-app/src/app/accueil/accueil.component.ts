68f99f3866c7819a6a188e933d6713ea7a945dcf
Domitien VISSOTSKY <domitien.vissotsky@student.vinci.be>
2020-12-04 11:22:50 +0100

    accueil component

---
+++ mobile-app/src/app/accueil/accueil.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {ApiService} from 'src/app/services/api/api.service'
+import {ICitizen} from 'src/models/citizen'
+import {IdbService} from '../services/idb.service'
+
+@Component({
+  selector: 'app-accueil',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.css'],
+})
+export class AccueilComponent {
+
+  citizen: ICitizen = { 
+    citizen_id: "non Disponible , Inscrivez vous !",
+    sick_since: null
+  }
+  constructor(private router: Router, private apiService :ApiService, private indexedDBService :IdbService) {
+   
+  }
+
+  titleSignIn: string = 'Enregistrez vous pour utiliser le QR Code !';
+  
+
+
+  scanhandler() {
+    if(localStorage.getItem("idCitizen")){
+      this.apiService.signIn().subscribe( data =>{ 
+        this.citizen = data;
+        localStorage.setItem("idCitizen", this.citizen.citizen_id.toString())
+        this.router.navigate(['/scanqr']);
+       })
+    }else{
+      this.router.navigate(['/signin']);
+    }
+    
+    
+    console.log(this.citizen)
+   
+    
+  }
+}
+
--- mobile-app/src/app/app-routing.module.ts
+++ mobile-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScanQrComponent } from './scan-qr/scan-qr.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
-  { path: '' , redirectTo: '/signin',pathMatch: 'full'},
+  { path: '' , redirectTo: '/accueil',pathMatch: 'full'},
   { path: 'signin' , component: SignInComponent},
-  { path: 'scanqr' , component: ScanQrComponent}
+  { path: 'scanqr' , component: ScanQrComponent},
+  { path: 'accueil' , component: AccueilComponent}
 ];
 
 @NgModule({
@@ -14,4 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ScanQrComponent,SignInComponent]
+export const routingComponents = [AccueilComponent,ScanQrComponent,SignInComponent]

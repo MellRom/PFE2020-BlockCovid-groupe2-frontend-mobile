c1293102b420cdf9510aa02c2548496b3b3dd8de
Melvin Villanueva <melvin.villanueva@student.vinci.be>
2020-12-09 16:32:30 +0100

    push notification specific user

--- mobile-app/src/app/app.component.ts
+++ mobile-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SwPush } from '@angular/service-worker';
 import { WebSocketService } from './services/webSocketService/web-socket-service.service';
 
 @Component({
@@ -11,35 +10,19 @@ import { WebSocketService } from './services/webSocketService/web-socket-service
 export class AppComponent implements OnInit {
   title = 'BlockCovid';
   private readonly websocketService!: WebSocketService;
-  //public notification : string = ""
   constructor(
     private router: Router,
-    private swPush: SwPush,
     private webSocketService: WebSocketService
   ) {
     let stompClient = this.webSocketService.connect();
+    let id = localStorage.getItem('uuid-citizen');
     stompClient.connect({}, (frame) => {
-      /*  stompClient.subscribe('/user/covid/notification', notifications => {
-          console.log(notifications)
-       })*/
-      stompClient.subscribe('/covid/notification', (notifications) => {
-        console.log(notifications.body);
-        let id = localStorage.getItem('uuid-citizen');
-        let setId = JSON.parse(notifications.body);
-        console.log(setId);
-        for (let index = 0; index < setId.length; index++) {
-          console.log(setId[index]);
-          console.log(id);
-          if (setId[index] === id) {
-            console.log('coucou');
-            //  this.notification = "Vous avez été récemment en contact avec une personne malade,\nMerci de prendre les mesures necessaires"
-            alert(
-              'Vous avez été récemment en contact avec une personne malade,\nMerci de prendre les mesures necessaires'
-            );
-            //   return;
-          }
+      stompClient.subscribe(
+        '/user/' + id + '/covid/notification',
+        (notifications) => {
+          alert(notifications.body);
         }
-      });
+      );
     });
   }
   onClickHandler() {
